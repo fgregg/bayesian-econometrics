97bf87752994a7d5d1fa736469da27c0eb2fbbb4
Forest Gregg <fgregg@uchicago.edu>
2013-04-16 15:42:12 -0500

    class examples

---
+++ class_examples/MCerror-class3.R
@@ -0,0 +1,63 @@
+#################################################################
+#
+#  Using the bivariate normal to illustrate the effect
+#  of unnecessary MC steps, or simply the Raoblackwellization
+#  of MC methods.
+#
+#################################################################
+set.seed(1547)
+mux   = 1
+muy   = 2
+sig2x = 1
+sig2y = 1
+sigxy = 0.75
+sigx  = sqrt(sig2x)
+sigy  = sqrt(sig2y)
+sdy.x = sqrt(sig2y-sigxy^2/sig2x)
+
+
+# Showing the difference in MC errors for both estimators 
+# of the marginal mean
+par(mfrow=c(1,3))
+for (M in c(100,1000,10000)){
+  E1 = rep(0,1000)
+  E2 = rep(0,1000)
+  for (i in 1:1000){
+    x = rnorm(M,mux,sigx)
+    y = muy + sigxy/sig2x*(x-mux) + sdy.x*rnorm(M)
+    E1[i] = muy + sigxy/sig2x*(mean(x)-mux)
+    E2[i] = mean(y)
+  }
+  boxplot(E2,E1,names=c("No Raoblackwellization","Raoblackwellization"),outline=FALSE,
+          main=paste("MC size=",M,sep=""))
+}
+
+
+
+# Showing the difference in MC errors for both estimators 
+# of the marginal density
+M     = 1000
+ys    = seq(-3,7,length=100)
+deny  = matrix(0,1000,100)
+y     = matrix(0,1000,M)
+for (j in 1:1000){
+   x = rnorm(M,mux,sigx)
+   y[j,] = muy + sigxy/sig2x*(x-mux) + sdy.x*rnorm(M)
+   for (i in 1:100)
+     deny[j,i] = mean(dnorm(ys[i],muy + sigxy/sig2x*(x-mux),sdy.x))
+}
+
+par(mfrow=c(1,2))
+plot(density(y[1,]),col=grey(0.8),ylim=c(0,0.5),main="No Raoblackwellization",xlab="")
+for (j in 2:1000)
+  lines(density(y[j,]),col=grey(0.8))
+lines(ys,dnorm(ys,2,1),lwd=2)
+
+plot(ys,deny[1,],type="l",col=grey(0.8),ylim=c(0,0.5),
+     main="Raoblackwellization",xlab="",ylab="Density")
+for (j in 2:1000)
+  lines(ys,deny[j,],col=grey(0.8))
+lines(ys,dnorm(ys,2,1),lwd=2)
+
+
+
---
+++ class_examples/student-t-simple-regression-class2.R
@@ -0,0 +1,59 @@
+############################################################
+#
+#  y(i) = alpha + beta*x(i) + e(i)  
+#
+#  where e(i) are iid Student t with nu degrees of freedom
+#
+#
+############################################################
+set.seed(13454)
+n = 500
+v = 3
+alpha = 0
+beta = 1
+sig2 = 1
+e = rt(n,v)
+x = 5*runif(n)
+y = alpha + beta*x + e
+
+par(mfrow=c(1,2))
+plot(x,y,main="Notice the size of the extreme events")
+hist(y,breaks=seq(min(y),max(y),length=20),prob=TRUE)
+lines(sort(y),dnorm(sort(y),mean(y),sqrt(var(y))),col=2,lwd=2)
+
+# Log posterior distribution
+logpost = function(beta){
+-beta^2/200 -5.5*sum(log(1+(y-alpha-beta*x)^2/(10*sig2)))
+}
+
+# Enveloping the posterior density with q=N(1,0.05^3)
+betas = seq(0.5,1.3,length=500)
+lp = rep(0,500)
+q  = rep(0,500)
+for (i in 1:500){
+  lp[i] = logpost(betas[i])
+  q[i]  = dnorm(betas[i],1,0.05)
+}
+
+par(mfrow=c(1,1))
+plot(betas,exp(lp-max(lp)),type="l",xlab=expression(beta),ylab="")
+lines(betas,q/max(q),col=2)
+
+
+# drawing from the proposal - N(0.94,0.12^2)
+betas1 = rnorm(10000,1,0.05)
+
+# Computing SIR weights
+w = rep(0,10000)
+for (i in 1:10000)
+  w[i] = logpost(betas1[i])-dnorm(betas1[i],1,0.05,log=TRUE)
+w = exp(w-max(w))
+w = w/sum(w)
+
+# Resampling
+betas2 = sample(betas1,size=1000,replace=TRUE,prob=w)
+
+par(mfrow=c(1,1))
+hist(betas2,xlim=range(betas2),breaks=seq(min(betas2),max(betas2),length=20),prob=TRUE)
+lines(betas,20*exp(lp-max(lp)),col=2)
+
